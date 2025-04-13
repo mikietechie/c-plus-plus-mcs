21911462e6bd7c6d82c96c1003ef305845fbc070
mikietechie <mzinyoni7@outlook.com>
2025-04-13 13:29:09 +0300

    done with 1 h

--- lesson_1/task_h/main.cpp
+++ lesson_1/task_h/main.cpp
@@ -1,45 +1,41 @@
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int count(string str, char chr)
-{
-    int k = 0;
-    for (char char_i : str)
-    {
-        if (char_i == chr)
-        {
-            k++;
-        }
-    }
-    return k;
-}
-
-char find_most_frequent_letter(string word)
-{
-    char output;
-    int max = 0;
-    for (char letter : word)
-    {
-        if (letter == ' ') {
-            continue;
-        }
-        int letter_count = count(word, letter);
-        if (letter_count > max)
-        {
-            output = letter;
-            max = letter_count;
-        }
-    }
-    return output;
-}
 
 int main(int argc, char const *argv[])
 {
     int length;
     std::cin >> length;
-    string word;
-    std::cin >> word;
-    std::cout << find_most_frequent_letter(word) << endl;
+    if (length == 0)
+    {
+        return 0;
+    }
+    vector<int> digits;
+    for (int i = 0; i < length; i++)
+    {
+        int digit;
+        std::cin >> digit;
+        digits.push_back(digit);
+    }
+    int most_frequent_digit = digits[0];
+    int most_frequent_digit_count = 1;
+    for (auto digit : digits)
+    {
+        int digit_count = 0;
+        for (auto _digit : digits)
+        {
+            if (digit == _digit)
+            {
+                digit_count++;
+            }
+        }
+        if (digit_count > most_frequent_digit_count)
+        {
+            most_frequent_digit_count = digit_count;
+            most_frequent_digit = digit;
+        }
+    }
+    std::cout << most_frequent_digit << endl;
     return 0;
 }
