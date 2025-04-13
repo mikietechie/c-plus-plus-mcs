6ac6ff093f405a9f115043cad824de8c56a3e978
mikietechie <mzinyoni7@outlook.com>
2025-04-13 14:05:34 +0300

    i

--- lesson_1/task_i/main.cpp
+++ lesson_1/task_i/main.cpp
@@ -1,17 +1,42 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-char func(string input)
-{
-}
 
 int main(int argc, char const *argv[])
 {
     int length;
     std::cin >> length;
-    string input;
-    std::cin >> input;
-    std::cout << func(input) << endl;
+    if (length == 0)
+    {
+        return 0;
+    }
+    int first_digit;
+    vector<int> digits;
+    for (int i = 0; i < length; i++)
+    {
+        int digit;
+        std::cin >> digit;
+        digits.push_back(digit);
+        if (i == 0)
+        {
+            first_digit = digit;
+        }
+    }
+    sort(digits.rbegin(), digits.rend());
+    // reverse(digits.rbegin(), digits.rend());
+
+    // std::cout << "..........." << std::endl;
+    // std::cout << first_digit << std::endl;
+    for (int i = 0; i < length; i++)
+    {
+        // std::cout << i+1 << "\t:\t" << digits[i] << std::endl;
+        if (digits[i] == first_digit)
+        {
+            std::cout << length-i << std::endl;
+            break;
+        }
+    }
     return 0;
 }
