a16c6fe54a6f809a0f979f2a63235d55388f49c7
mikietechie <mzinyoni7@outlook.com>
2025-04-13 14:19:26 +0300

    1j

--- lesson_1/task_j/main.cpp
+++ lesson_1/task_j/main.cpp
@@ -1,26 +1,41 @@
-#include <string>
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-
-string func(string input)
-{
-    int input_length = input.length();
-    int n_over_2 = input_length / 2;
-    string start = input.substr(0, n_over_2);
-    string end = input.substr(n_over_2, input_length);
-    reverse(end.begin(), end.end());
-    string output = start + end;
-    return output;
-}
 
 int main(int argc, char const *argv[])
 {
     int length;
     std::cin >> length;
-    string input;
-    std::cin >> input;
-    std::cout << func(input) << endl;
+    int half = length / 2;
+    vector<int> digits1;
+    vector<int> digits2;
+    for (int i = 0; i < length; i++)
+    {
+        int digit;
+        std::cin >> digit;
+        if (i < half)
+        {
+            digits1.push_back(digit);
+        }
+        else
+        {
+            digits2.push_back(digit);
+        }
+    }
+    sort(digits1.rbegin(), digits1.rend());
+    reverse(digits1.rbegin(), digits1.rend());
+    sort(digits2.rbegin(), digits2.rend());
+
+    for (auto digit : digits1)
+    {
+        std::cout << digit << ' ';
+    }
+
+    for (auto digit : digits2)
+    {
+        std::cout << digit << ' ';
+    }
+    std::cout << std::endl;
     return 0;
 }
