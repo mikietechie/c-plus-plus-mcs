5f794046084af97f63f3517377600e11d2210e06
mikietechie <mzinyoni7@outlook.com>
2025-04-13 15:22:52 +0300

    4a

--- lesson_4/a/main.cpp
+++ lesson_4/a/main.cpp
@@ -1,8 +1,30 @@
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    int length;
+    std::cin >> length;
+    vector<int> digits;
+    int maximum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        int digit;
+        std::cin >> digit;
+        digits.push_back(digit);
+        if (digit > maximum)
+        {
+            maximum = digit;
+        }
+    }
+    sort(digits.rbegin(), digits.rend());
+    reverse(digits.rbegin(), digits.rend());
+    for (int digit : digits)
+    {
+        std::cout << digit << ' ';
+    }
+    std::cout << std::endl;
     return 0;
 }
